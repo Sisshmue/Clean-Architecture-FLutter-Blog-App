5c2d270528af8cc8379fbc1b7e6a96bef5eadade
sisshmue <sisshmue404@gmail.com>
2025-12-29 22:49:22 +0700

    create signIn and logIn pages

---
+++ lib/features/auth/presentaion/pages/login_page.dart
@@ -0,0 +1,83 @@
+import 'package:blog_app_clean_architecture/core/theme/app_pallete.dart';
+import 'package:blog_app_clean_architecture/features/auth/presentaion/pages/signup_page.dart';
+import 'package:blog_app_clean_architecture/features/auth/presentaion/widges/auth_field.dart';
+import 'package:blog_app_clean_architecture/features/auth/presentaion/widges/auth_gradient_button.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static MaterialPageRoute<dynamic> route() =>
+      MaterialPageRoute(builder: (context) => LoginPage());
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //formKey.currentState!.validate();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign In.',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30),
+              AuthField(hintText: 'Email', controller: emailController),
+              SizedBox(height: 15),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              SizedBox(height: 20),
+              AuthGradientButton(buttonText: 'Sign in'),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, SignupPage.route());
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Don\'t Have an account?  ',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                      TextSpan(
+                        text: 'Sign Up',
+                        style: Theme.of(context).textTheme.titleMedium
+                            ?.copyWith(
+                              color: AppPallete.gradient2,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
